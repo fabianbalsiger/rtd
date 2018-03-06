cb14c6fa2c081ef97072b61816d4d19e7e26fb8b
Fabian <accounts@fabianbalsiger.ch>
2018-03-06 20:41:06 +0100

    docs(conf): change exclude patterns

--- docs/conf.py
+++ docs/conf.py
@@ -89,7 +89,7 @@ language = None
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # These patterns also affect html_static_path and html_extra_path
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', 'setup.py', 'test']
 
 # The reST default role (used for this markup: `text`) to use for all
 # documents.
